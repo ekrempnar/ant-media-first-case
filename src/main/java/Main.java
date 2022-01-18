9d975eaae77e04e31e577b7db476c85abe004610
Ekrem Pınar <ekrem.pinar@otelz.com>
2022-01-18 14:12:04 +0300

    add jacoco for sonar cloud code coverage

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Calculate calculate = new Calculate();
-        System.out.println(calculate.sumTwoNumbers(5, 5));
-    }
-}
